88735639a7bc0b8866b0d9a852abffa1410dc8b0
Adam Liang <hello@adamliang.dev>
2024-03-19 04:37:03 -0400

    docs: added documentationt to color.rs

--- examples/cli-utils/src/colors.rs
+++ examples/cli-utils/src/colors.rs
@@ -5,6 +5,9 @@
 //! println!("{}{}{}", red("Red"), green("Green"), blue("Blue"));
 //! ```
 
+// The purpose of documenting code is to help others understand it.
+// 
+
 /// Returns a string with the ANSI escape code for red.
 /// # Examples:
 /// ```
@@ -14,23 +17,52 @@
 pub fn red(s: &str) -> String {
     format!("\x1b[31m{}\x1b[0m", s)
 }
-
+/// Returns a string with the ANSI escape code for green.
+/// # Examples:
+/// ```
+/// use cli_utils::colors::*;
+/// println!("{}", green("Green"));
+/// ```
 pub fn green(s: &str) -> String {
     format!("\x1b[32m{}\x1b[0m", s)
 }
 
+/// Returns a string with the ANSI escape code for blue.
+/// # Examples:
+/// ```
+/// use cli_utils::colors::*;
+/// println!("{}", blue("Blue"));
+/// ```
 pub fn blue(s: &str) -> String {
     format!("\x1b[34m{}\x1b[0m", s)
 }
 
+/// Returns a string with the ANSI escape code for bold.
+/// # Examples:
+/// ```
+/// use cli_utils::colors::*;
+/// println!("{}", bold("Bold"));
+/// ```
 pub fn bold(s: &str) -> String {
     format!("\x1b[1m{}\x1b[0m", s)
 }
 
+/// Returns a string with the ANSI escape code to reset the color.
+/// # Examples:
+/// ```
+/// use cli_utils::colors::*;
+/// println!("{}", reset("Reset"));
+/// ```
 pub fn reset(s: &str) -> String {
     format!("\x1b[0m{}\x1b[0m", s)
 }
 
+/// An enum to represent the color of a string.
+/// # Examples: 
+/// ```
+/// use cli_utils::colors::Color;
+/// let color = Color::Red;
+/// ```
 pub enum Color{
     Red,
     Green,
@@ -38,6 +70,7 @@ pub enum Color{
     Bold,
 }
 
+/// A struct to represent a string with a color.
 pub struct ColorString {
     pub color: Color,
     pub string: String,
